ff873e67ba2a81e55c7037b48cf259027966b5a9
Angelito Casagrande <angelito@angelito.com.br>
2020-05-01 17:27:32 -0300

    Add page counter

---
+++ CounterPage/src/App/index.tsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import * as signalR from "@microsoft/signalr";
+
+interface ICounter {
+  hahaCounter?: number;
+  heartCounter?: number;
+  likeCounter?: number;
+  tongueCounter?: number;
+}
+
+const App = () => {
+  const [counters, setCounters] = useState<ICounter>({});
+  const [hubConnection, setHubConnection] = useState<signalR.HubConnection>();
+  const [hubStarted, setHubStarted] = useState(false);
+
+  useEffect(() => {
+    setHubConnection(
+      new signalR.HubConnectionBuilder()
+        .withUrl("https://localhost:5001/clickcounter")
+        .build()
+    );
+  }, []);
+
+  useEffect(() => {
+    if (!!hubConnection && !hubStarted) {
+      hubConnection
+        .start()
+        .then(() => {
+          setHubStarted(true);
+          console.log("connected");
+          hubConnection.on("SendClickCounters", (counter: ICounter) => {
+            setCounters(counter);
+          });
+        })
+        .catch((err) => console.log({ err }));
+    }
+  }, [hubConnection, hubStarted]);
+
+  const handleLikeClick = () =>
+    hubConnection?.invoke("LikeClick", hubConnection?.connectionId);
+
+  const handleHahaClick = () =>
+    hubConnection?.invoke("HahaClick", hubConnection?.connectionId);
+
+  const handleTongueClick = () =>
+    hubConnection?.invoke("TongueClick", hubConnection?.connectionId);
+
+  const handleHeartClick = () =>
+    hubConnection?.invoke("HeartClick", hubConnection?.connectionId);
+
+  return (
+    <div className="group">
+      <div className="hit">This is the SignalR in action</div>
+      <div className="hit">Click Me!</div>
+      <div className="box-container row">
+        <div className="box-item col-xs-6" onClick={handleLikeClick}>
+          <i className="fas fa-thumbs-up"></i>
+          <span className="counter-like">{counters.likeCounter || 0}</span>
+        </div>
+        <div className="box-item col-xs-6" onClick={handleHahaClick}>
+          <i className="fas fa-laugh"></i>
+          <span className="counter-like">{counters.hahaCounter || 0}</span>
+        </div>
+        <div className="box-item col-xs-6" onClick={handleHeartClick}>
+          <i className="fas fa-heart"></i>
+          <span className="counter-like">{counters.heartCounter || 0}</span>
+        </div>
+        <div className="box-item col-xs-6" onClick={handleTongueClick}>
+          <i className="fas fa-grin-tongue-wink"></i>
+          <span className="counter-like">{counters.tongueCounter || 0}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
